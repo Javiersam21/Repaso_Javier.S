1a4952a1fff8b46c7f2fc66dd4a903bf909ca76c
ALUMNOS <ALUMNOS@P622.AULAS.COM>
2024-05-08 12:16:27 +0200

    paco

---
+++ Javier_Repaso/src/Javier.java
@@ -0,0 +1,10 @@
+
+public class Javier {
+
+	public static void main(String[] args) {
+		// Javier.San Emeterio
+		System.out.println("Paco Osuna es el mejor DJ");
+
+	}
+
+}
--- Paco/src/Paco/Paco.java
+++
@@ -1,7 +0,0 @@
-package Paco;
-
-public class Paco {
-public static void main(String []arg) {
-	System.out.println("Paco Osuna es el mejor DJ");
-}
-}
